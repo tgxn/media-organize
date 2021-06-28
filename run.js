2ba7066367031bc78c64fa54686894a6c3af4138
tgxn <tgxn@tgxn.net>
2021-06-28 16:42:49 +0200

    No longer require config file in config class

--- run.js
+++ run.js
@@ -1,7 +1,9 @@
 const { Organize } = require('./lib/organize')
 
+const configFile = require('./config.json');
+
 async function run() {
-    let organizer = new Organize();
+    let organizer = new Organize(configFile);
     await organizer.organizeAll();
 }
 
