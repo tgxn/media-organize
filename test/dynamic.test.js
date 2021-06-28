9dcedb27930d54b59c27d42cd6b101d6dc9d466b
tgxn <tgxn@tgxn.net>
2021-06-28 16:38:07 +0200

    Add GitHub tests

--- test/dynamic.test.js
+++ test/dynamic.test.js
@@ -1,15 +1,14 @@
 const fs = require('fs').promises;
 const path = require('path');
 
-const {
-    runOrganize,
-    getFiles,
-    isAllowedFile,
-    isMovieOrSeries,
-    determineMediaInfo,
-    formatSeriesPath,
-    createSymlink,
-} = require('../lib/organize')
+const { Organize, OrganizerLayer } = require('../lib/organize')
+
+const mockedStore = {
+    findLinkWithSource: () => { },
+    deleteLink: () => { },
+    findLink: () => { },
+    createLink: () => { },
+};
 
 const configArray = require('../config.example.json');
 
@@ -22,11 +21,16 @@ async function performFileTests(testDataPath, configObject) {
 
             const parsedPath = path.parse(testFilePath);
 
-            const fileIsMedia = await isAllowedFile(testFilePath, configObject);
+            const organizeLayer = new OrganizerLayer({
+                store: mockedStore,
+                configArray
+            }, 0);
 
-            const movieOrSeries = await isMovieOrSeries(parsedPath);
+            const fileIsMedia = await organizeLayer.isAllowedFile(testFilePath, configObject);
 
-            const fileMediaInfo = await determineMediaInfo(parsedPath.name, movieOrSeries)
+            const movieOrSeries = await organizeLayer.isMovieOrSeries(parsedPath);
+
+            const fileMediaInfo = await organizeLayer.determineMediaInfo(parsedPath.name, movieOrSeries)
 
             return {
                 isAllowedFile: fileIsMedia,
@@ -46,29 +50,39 @@ async function performFileTests(testDataPath, configObject) {
 test('isAllowedFile', async () => {
     let testResult;
 
-    testResult = await isAllowedFile("yhjkjk.tmp", configArray[0]);
+    const organizeLayer = new OrganizerLayer({
+        store: mockedStore,
+        configArray
+    }, 0);
+
+    testResult = await organizeLayer.isAllowedFile("yhjkjk.tmp");
     expect(testResult).toBe(false);
 
-    testResult = await isAllowedFile("fstgdh.mkv", configArray[0]);
+    testResult = await organizeLayer.isAllowedFile("fstgdh.mkv");
     expect(testResult).toBe(true);
 
-    testResult = await isAllowedFile("hrthwthaesrthaewthg", configArray[0]);
+    testResult = await organizeLayer.isAllowedFile("hrthwthaesrthaewthg");
     expect(testResult).toBe(null);
 
-    testResult = await isAllowedFile("", configArray[0]);
+    testResult = await organizeLayer.isAllowedFile("");
     expect(testResult).toBe(null);
 });
 
 test('isMovieOrSeries', async () => {
     let testResult;
 
-    testResult = await isMovieOrSeries(path.parse("Get.Rich.Or.Die.Tryin.2005.1080p.BluRay.REMUX.AVC.TrueHD.5.1-UnKn0wn.mkv"));
+    const organizeLayer = new OrganizerLayer({
+        store: mockedStore,
+        configArray
+    }, 0);
+
+    testResult = await organizeLayer.isMovieOrSeries(path.parse("Get.Rich.Or.Die.Tryin.2005.1080p.BluRay.REMUX.AVC.TrueHD.5.1-UnKn0wn.mkv"));
     expect(testResult).toBe("movie");
 
-    testResult = await isMovieOrSeries(path.parse("Invincible.2021.S01E08.Where.I.Really.Come.From.1080p.AMZN.WEB-DL.DDP5.1.H.264-NTb.mkv"));
+    testResult = await organizeLayer.isMovieOrSeries(path.parse("Invincible.2021.S01E08.Where.I.Really.Come.From.1080p.AMZN.WEB-DL.DDP5.1.H.264-NTb.mkv"));
     expect(testResult).toBe("series");
 
-    testResult = await isMovieOrSeries("");
+    testResult = await organizeLayer.isMovieOrSeries("");
     expect(testResult).toBe(null);
 
 });
@@ -84,7 +98,6 @@ test('run movie file tests', () => {
                 }
             }
 
-
         });
 });
 
@@ -98,7 +111,6 @@ test('run series file tests', () => {
                     expect(data[result].movieOrSeries).toBe("series");
                 }
             }
-
 
         });
 });
